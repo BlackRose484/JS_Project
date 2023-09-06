c40454d9547c6e9d15dd5860396ed3aeacd9c348
hung <hungnbc2@gmai.com>
2023-09-06 12:54:27 +0700

    Connect Db Atlas

--- src/server.js
+++ src/server.js
@@ -35,6 +35,6 @@ route(app)
 
 const PORT = process.env.PORT||3000
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
